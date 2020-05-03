c4249b14aad9731bcc4b7449adb72c1bd03cb3fc
naokinakai <creator.nakai@gmail.com>
2020-05-04 02:03:15 +0900

    cssをstyle.cssにまとめました。ベーシック認証を外しました。

--- inc/head-css.php
+++ inc/head-css.php
@@ -1,11 +1,2 @@
 <link rel="shortcut icon" href="/img/favicon.ico">
-<!--<link rel="stylesheet" href="/css/style.css">-->
-<link rel="stylesheet" href="/css/reset.css">
-<link rel="stylesheet" href="/css/common.css">
-<link rel="stylesheet" href="/css/page.css">
-<link rel="stylesheet" href="/css/page320.css">
-<link rel="stylesheet" href="/css/page480.css">
-<link rel="stylesheet" href="/css/page768.css">
-<link rel="stylesheet" href="/css/page960.css">
-<link rel="stylesheet" href="/css/page1280.css">
-<link rel="stylesheet" href="/css/script.css">
+<link rel="stylesheet" href="/css/style.css">
